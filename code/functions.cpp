35ad43a4e85bd6ea642c565537eb1891b042ab63
connormaass <connormaass2@gmail.com>
2026-06-18 16:57:38 -0500

    some errors and main concept

--- code/functions.cpp
+++ code/functions.cpp
@@ -1,72 +1,152 @@
+#include <functional>
 #include "robot.h"
 #include "arduino.h"
 
-Robot::Robot(){
+Motor::Motor(int forwardPin = -1, int backwardPin = -1)
+{
+  this->backwardPin = backwardPin;
+  this->forwardPin = forwardPin;
+}
 
+void Motor::stop()
+{
+  digitalWrite(forwardPin, LOW);
+  analogWrite(backwardPin, 0);
 }
-Robot::Robot(int rmfp, int rmbp, int lmfp, int lmbp){
-  rightMotorForwardPin = rmfp;
-  rightMotorBackwardPin = rmbp;
-  leftMotorForwardPin = lmfp;
-  leftMotorBackwardPin = lmbp;
+void Motor::forward(int speed)
+{
+  digitalWrite(backwardPin, LOW);
+  analogWrite(forwardPin, speed);
 }
-void Robot::forward(float seconds, int speed) {
-  digitalWrite(this->rightMotorBackwardPin, LOW);
-  analogWrite(this->rightMotorForwardPin, speed);
-  digitalWrite(this->leftMotorBackwardPin, LOW);
-  analogWrite(this->leftMotorForwardPin, speed);
+void Motor::backward(int speed)
+{
+  digitalWrite(forwardPin, LOW);
+  analogWrite(backwardPin, speed);
+}
+
+Instruction::Instruction(function<void(float, int)> func, int arg, float seconds)
+{
+  this->func = func;
+  this->arg = arg;
+  this->seconds = seconds;
+}
+
+void Instruction::run()
+{
+  func(arg);
+}
+
+float Instruction::getTime()
+{
+  return seconds;
+}
+
+Robot::Robot()
+{
+}
+
+Robot::Robot(int rmfp, int rmbp, int lmfp, int lmbp)
+{
+  rightMotor = Motor(rmfp, rmbp);
+  leftMotor = Motor(lmfp, lmbp);
+}
+
+void addInstruction(function<void(float, int)> func, int arg, float seconds)
+{
+  instructions[instructIndex] = Instruction(func, arg, seconds);
+}
+
+void Robot::forward(float seconds, int speed)
+{
+  rightMotor.forward(speed);
+  leftMotor.forward(speed);
   digitalWrite(27, HIGH);
-  delay(1000*seconds);
+  delay(1000 * seconds);
   digitalWrite(27, LOW);
 }
 
-void Robot::backward(float seconds, int speed) {
-  digitalWrite(this->rightMotorForwardPin, LOW);
-  analogWrite(this->rightMotorBackwardPin, speed);
-  digitalWrite(this->leftMotorForwardPin, LOW);
-  analogWrite(this->leftMotorBackwardPin, speed);
-  delay(1000*seconds);
+void Robot::backward(float seconds, int speed)
+{
+  rightMotor.backward(speed);
+  leftMotor.backward(speed);
+  delay(1000 * seconds);
 }
 
-void Robot::right(float seconds, int speed) {
-  digitalWrite(this->rightMotorBackwardPin, LOW);
-  analogWrite(this->rightMotorForwardPin, speed);
-  digitalWrite(this->leftMotorForwardPin, LOW);
-  analogWrite(this->leftMotorBackwardPin, 0);
+void Robot::right(float seconds, int speed)
+{
+  rightMotor.forward(speed);
+  leftMotor.stop();
+
   digitalWrite(27, HIGH);
-  delay(1000*seconds);
+  delay(1000 * seconds);
   digitalWrite(27, LOW);
 }
 
-void Robot::left(float seconds, int speed) {
-  digitalWrite(this->rightMotorForwardPin, LOW);
-  analogWrite(this->rightMotorBackwardPin, 0);
-  digitalWrite(this->leftMotorBackwardPin, LOW);
-  analogWrite(this->leftMotorForwardPin, speed);
-  delay(1000*seconds);
+void Robot::left(float seconds, int speed)
+{
+  rightMotor.stop();
+  leftMotor.forward(speed);
+  delay(1000 * seconds);
 }
 
 // Connor's stuff is below
 
-void Robot::stop(float seconds) {
-  digitalWrite(this->rightMotorBackwardPin, LOW);
-  analogWrite(this->rightMotorForwardPin, 0);
-  digitalWrite(this->leftMotorBackwardPin, LOW);
-  analogWrite(this->leftMotorForwardPin, 0);
-  delay(1000*seconds);
-  
+void Robot::stop()
+{
+  rightMotor.stop();
+  leftMotor.stop();
 }
 
-void Robot::simpleAccel(int setpoint, double seconds, double startTime){
+void Robot::simpleAccel(float seconds, int setpoint)
+{
   accelStartTime = startTime;
   accelTime = seconds;
   this->setpoint = setpoint;
 }
 
-void Robot::updateAccel(double currSeconds){
+void Robot::updateAccel(double currSeconds)
+{
   double mult = (currSeconds - accelStartTime) / accelTime;
-  //speed = setpoint * mult;
-
+  // speed = setpoint * mult;
 }
 
+void Robot::update()
+{
+}
 
+AbstractRobot::AbstractRobot()
+{
+  robot = Robot();
+}
+
+void AbstractRobot::forward(float seconds, int speed)
+{
+  robot.addInstruction(robot.forward, speed, seconds);
+}
+
+void AbstractRobot::backward(float seconds, int speed)
+{
+  robot.addInstruction(robot.backward, speed, seconds);
+}
+
+void AbstractRobot::right(float seconds, int speed)
+{
+  robot.addInstruction(robot.right, speed, seconds);
+}
+
+void AbstractRobot::left(float seconds, int speed)
+{
+  robot.addInstruction(robot.left, speed, seconds);
+}
+
+// Connor's stuff is below
+
+void AbstractRobot::stop()
+{
+  robot.addInstruction(robot.stop, speed, seconds);
+}
+
+void AbstractRobot::simpleAccel(float seconds, int setpoint)
+{
+  robot.addInstruction(robot.simpleAccel, speed, seconds);
+}
--- code/robot.h
+++ code/robot.h
@@ -8,32 +8,76 @@
 //   LEFT = 2,
 //   RIGHT = 3,
 // }
+#include <functional>
 
-class Robot{
-  public:
+// MOTOR CLASS
+class Motor
+{
+public:
+  Motor(int backwardPin = -1, int forwardPin = -1);
+  void stop();
+  void forward(int speed);
+  void backward(int speed);
+
+private:
+  int forwardPin;
+  int backwardPin;
+};
+
+// INSTRUCTION CLASS
+class Instruction
+{
+public:
+  Instruction(std::function<void(float, int)> func, int arg, float seconds);
+  void run();
+  int getTime();
+
+private:
+  std::function<void(float, int)> func;
+  int arg;
+  float seconds;
+}
+
+// ROBOT CLASS
+class Robot
+{
+public:
   Robot();
   Robot(int rmfp, int rmbp, int lmfp, int lmbp);
-  void forward(float seconds,int speed);
+  void forward(float seconds, int speed);
   void backward(float seconds, int speed);
   void right(float seconds, int speed);
   void left(float seconds, int speed);
-  void stop(float seconds);
-  void simpleAccel(int setpoint, double seconds, double startTime);
+  void stop();
+  void simpleAccel(float seconds, int setpoint);
   void updateAccel(double currSeconds);
-  private:
-  int rightMotorForwardPin;
-  int rightMotorBackwardPin;
-  int leftMotorForwardPin;
-  int leftMotorBackwardPin;
-  double accelStartTime = 0;
-  double accelTime = 0;
+  void addInstruction(function<void(float, int)> func, int arg, float seconds);
+
+private:
+  Motor rightMotor;
+  Motor leftMotor;
+  Instruction instructions[10];
+  double funcStartTime = 0;
+  double funcTime = 0;
   int setpoint = 0;
   bool accelerate = false;
+  int instructIndex = 0;
   // enum Direction currDir = NONE;
-
 };
 
+// ABSTRACT ROBOT CLASS
+class AbstractRobot // what CCCC will interact with
+{
+public:
+  AbstractRobot();
+  void forward(float seconds, int speed);
+  void backward(float seconds, int speed);
+  void right(float seconds, int speed);
+  void left(float seconds, int speed);
+  void stop();
+  void simpleAccel(float seconds, int setpoint);
 
-
+  priavte : Robot robot;
+}
 
 #endif
