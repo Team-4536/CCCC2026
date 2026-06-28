002e310258a64e0ffb4adf2228d5caba07d1cf89
connormaass <connormaass2@gmail.com>
2026-06-27 21:54:31 -0500

    make their own function

--- code/main/main.ino
+++ code/main/main.ino
@@ -2,26 +2,41 @@
 #include "robot.hpp"
 #include "arduino.h"
 
+void turn180();
 AbstractRobot robot;
+
+// Your number from level 3 here:
+const float unnamedVar;
 
 void fakeMain()
 {
-  // put your main code here, instructions for the robot:
-  // functions:
-  // 1. forward : takes a time in seconds and a speed
-  // 2. backward : takes a time in seconds and a speed
-  // 3. right : takes a time in seconds and a speed
-  // 4. left : takes a time in seconds and a speed
-  // 5. stop : takes no arguments
-  // 6. accelerate : takes a time in seconds and a speed to accelerate to
-  robot.accelerate(5, 255);
-  robot.forward(5, 255);
-  robot.stop();
-  robot.wait(3);
-  robot.backward(5, 255);
-  robot.right(2, 255);
-  robot.left(2, 255);
+  // Let's turn around 180 degrees!
+
+  // You'll need the number you found from the previous level
+  //    - Practice writing your own variable name!
+
+  // To make the robot turn around 180 degrees we have a few options
+  //  - We could try calling the right or left method and double the time we put int
+  //    - Something like robot.right(yourVar * 2, 255);
+  //  - OR we could call the right or left method twice
+
+  // Lets do the second option with some modification
+  // Calling a method twice should work, but repeating it is totally way to much work
+  //  - Actually though, repeating lines in a big sytem can crowd and complicate code
+
+  // So lets make your very own function!
+
+  // Instructions:
+  // 1. Navigate a few lines down to find Definiton of the turn180() function
+  // 2. In the function, write code to turn the robot 180 degrees by calling right or left twice
+  // 3. Use your new function below in to move forward, turn around, and move forward
+  //  - Fun fact: You are writing code in fakeMain() which is also a function! (Function Inception if you will)
+  
 }
+
+void turn180(){
+
+};
 
 // Dont worry about this stuff for now :D
 
