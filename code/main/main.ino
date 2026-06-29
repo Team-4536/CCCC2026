e78fb3b36fbde32d8d5a2de7e6be654795b9b15c
connormaass <connormaass2@gmail.com>
2026-06-29 13:51:13 -0500

    yap sesh, this one is for loops

--- code/main/main.ino
+++ code/main/main.ino
@@ -6,36 +6,64 @@ void turn180();
 AbstractRobot robot;
 
 // Your number from level 3 here:
-const float unnamedVar;
+//  -Oh no! looks like I forogt to type the varible, what would be good at storing a decimal?
+const float TURN_90; // TODO change float to ??? to have them type it themselves
 
 void fakeMain()
 {
-  // Let's turn around 180 degrees!
+  // Lets make the robot spin around a BUNCH
 
-  // You'll need the number you found from the previous level
-  //    - Practice writing your own variable name!
+  // You'll need your number from level 3 put into the variable at the top
+  
+  // The turn180() function wasn't a huge pain to make because you only had to copy and paste the right (or left) command twice
+  // But what if I asked you to turn 5 million degrees? Or less extreme, make the robot spin around 5 times
+  // You could make a custom function called "spinAround5Tims" and copy and paste the turn 90 degree command 20 times...
+  // Or we could use a loop! Turn 90 degrees, 20 times
+  //  - If you don't want to know all the nitty gritty of a for loop, skip ahead to the instructions at the bottom of fakeMain()
 
-  // To make the robot turn around 180 degrees we have a few options
-  //  - We could try calling the right or left method and double the time we put int
-  //    - Something like robot.right(yourVar * 2, 255);
-  //  - OR we could call the right or left method twice
+  // All loops start with the while loop, everthing in the brackets get repeated as long as the condition (called a Boolean) is true
 
-  // Lets do the second option with some modification
-  // Calling a method twice should work, but repeating it is totally way to much work
-  //  - Actually though, repeating lines in a big sytem can crowd and complicate code
+  while(true){
+    break; // I don't actually want to run this loop here so I break it early
+  }
 
-  // So lets make your very own function!
+  // What we need to do though is check how many times we have looped or iterated through the code in the loop so here's how we do that
 
-  // Instructions:
-  // 1. Navigate a few lines down to find Definiton of the turn180() function
-  // 2. In the function, write code to turn the robot 180 degrees by calling right or left twice
-  // 3. Use your new function below in to move forward, turn around, and move forward
-  //  - Fun fact: You are writing code in fakeMain() which is also a function! (Function Inception if you will)
+  int i = 0; // We'll make an int variable and name it i for iteration, it will track which loop we are on
+
+  while(i < 5){ // This is our condition
+    // do code
+    i++; // This is shorthand notation for adding 1 to a variable
+  }
+
+  // This will run the loop 5 times and here's how:
+  //  - Before every execution of code, the loop will check to see if the condition is true
+  //  - Since i starts at 0 and 0 is les than 5, the condition passes and the code runs
+  //  - Part of the code is to add 1 to the i variable, making it 1 after the first iteration
+  //  - Then the loop will check the condition again, i is now 1 but 1 is still less than 5 so it will pass
+  //  - This will continue and to track, i is 0: first loop, i is 1: second loop, i is 2: third loop, i is 3: fourth loop
+  //  - When i is 4, the condition will still pass, running the code a 5th time. 
+  //  - But now i is 5. 5 is not less than 5 so the loop stops running and our code ran a grand total of 5 times
+  
+  // Congratulations! you now know how a for loop works, so lets make one
+  // A for loop has different sytax from what you might excpect but it works the same
+  //  - All a for loop's syntax does differently is having the tracking variable be inside some parentheses
+
+
+  // Instructions: Using the loop below, make the robot spin 360 degrees around 5 times
+
+  int loops; // Set this to the amount of times you want your loop to run
+
+  for(int i = 0; i < loops; i++){ 
+    // Your code goes here
+  }  
   
 }
 
+// Your function may have worked slightly differently and if it ain't broke: don't fix it
 void turn180(){
-
+  robot.right(TURN_90);
+  robot.right(TURN_90);
 };
 
 // Dont worry about this stuff for now :D
