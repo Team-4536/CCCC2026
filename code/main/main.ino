44fef641d50f6f727ad5ad4c3aac926c3f28d407
connormaass <connormaass2@gmail.com>
2026-06-30 16:03:09 -0500

    doesn't seem to be working

--- code/main/main.ino
+++ code/main/main.ino
@@ -1,28 +1,14 @@
-
-
-const int right_enable_pin = 6;
-const int right_dir_pin = 7;
-const int left_enable_pin = 8;
-const int left_dir_pin = 9;
-const uint LED_Pin = 27;
-
 void setup() {
   // put your setup code here, to run once:
-  pinMode(right_enable_pin, OUTPUT);
-  pinMode(right_dir_pin, OUTPUT);
-  pinMode(left_enable_pin, OUTPUT);
-  pinMode(left_dir_pin, OUTPUT);
-  digitalWrite(right_dir_pin, HIGH);
-  digitalWrite(left_dir_pin, HIGH);
-  analogWrite(right_enable_pin, 255);
+  pinMode(LED_BUILTIN, OUTPUT);
 
 }
 
 void loop() {
 
-  analogWrite(left_enable_pin, 255);
+  digitalWrite(LED_BUILTIN, HIGH);
   delay(500);
-  analogWrite(left_enable_pin, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
   delay(500);
 
 }
