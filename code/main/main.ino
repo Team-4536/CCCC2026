ffec1543e54979e02591c02c243f1fd20dd1e2c1
connormaass <connormaass2@gmail.com>
2026-06-30 16:17:00 -0500

    yup it works

--- code/main/main.ino
+++ code/main/main.ino
@@ -2,27 +2,21 @@
 
 const int right_enable_pin = 6;
 const int right_dir_pin = 7;
-const int left_enable_pin = 8;
-const int left_dir_pin = 9;
-const uint LED_Pin = 27;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(right_enable_pin, OUTPUT);
   pinMode(right_dir_pin, OUTPUT);
-  pinMode(left_enable_pin, OUTPUT);
-  pinMode(left_dir_pin, OUTPUT);
+
   digitalWrite(right_dir_pin, HIGH);
-  digitalWrite(left_dir_pin, HIGH);
-  analogWrite(right_enable_pin, 255);
 
 }
 
 void loop() {
 
-  analogWrite(left_enable_pin, 255);
+  digitalWrite(right_enable_pin, HIGH);
   delay(500);
-  analogWrite(left_enable_pin, LOW);
+  digitalWrite(right_enable_pin, LOW);
   delay(500);
 
 }
