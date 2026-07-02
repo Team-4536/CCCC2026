f41adf32958af89d26c61e539558ba96e534d13e
connormaass <connormaass2@gmail.com>
2026-07-02 17:52:55 -0500

    Number change and helpful comments

--- code/main/main.ino
+++ code/main/main.ino
@@ -1,27 +1,28 @@
-const int right_enable_pin = 6;
-const int right_dir_pin = 7;
-const int left_enable_pin = 8;
-const int left_dir_pin = 9;
+int right_motor_enable_pin = 6;
+int right_motor_dir_pin = 7;
+int left_motor_enable_pin = 8;
+int left_motor_dir_pin = 9;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(right_enable_pin, OUTPUT);
-  pinMode(right_dir_pin, OUTPUT);
-  pinMode(left_enable_pin, OUTPUT);
-  pinMode(left_dir_pin, OUTPUT);
+  pinMode(right_motor_enable_pin, OUTPUT);
+  pinMode(right_motor_dir_pin, OUTPUT);
+  pinMode(left_motor_enable_pin, OUTPUT);
+  pinMode(left_motor_dir_pin, OUTPUT);
 
-  digitalWrite(right_dir_pin, HIGH);
-  digitalWrite(left_dir_pin, HIGH);
+  digitalWrite(right_motor_dir_pin, HIGH); // Setting the right motor direction to forward
+  digitalWrite(left_motor_dir_pin, HIGH); // Setting the left motor direction to forward
 
 }
 
 void loop() {
 
-  analogWrite(right_enable_pin, 255);
-  analogWrite(left_enable_pin, 255);
+  analogWrite(right_motor_enable_pin, 255); // This functions the exact same as a digitalWrite() on HIGH
+  analogWrite(left_motor_enable_pin, 255);
   delay(1000);
-  analogWrite(right_enable_pin, LOW);
-  analogWrite(left_enable_pin, LOW);
+
+  analogWrite(right_motor_enable_pin, 0);
+  analogWrite(left_motor_enable_pin, 0);
   delay(1000);
 
 }
