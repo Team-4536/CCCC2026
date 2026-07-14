7f3e5633b21e3a146d219b2f93461eb6725f3cfd
connormaass <connormaass2@gmail.com>
2026-07-14 16:49:11 -0500

    spin in a rough circle for wire testing

--- code/main/main.ino
+++ code/main/main.ino
@@ -1,20 +1,27 @@
 int right_enable_pin = 6;
 int right_dir_pin = 7;
+int left_enable_pin = 8;
+int left_dir_pin = 9;
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+
   pinMode(right_enable_pin, OUTPUT);
   pinMode(right_dir_pin, OUTPUT);
+  pinMode(left_enable_pin, OUTPUT);
+  pinMode(left_dir_pin, OUTPUT);
 
-  digitalWrite(right_dir_pin, HIGH); // For an extra challenge, make the motor move backward
-
-}
-
-void loop() {
+  digitalWrite(right_dir_pin, HIGH);
+  digitalWrite(left_dir_pin, HIGH);
 
   digitalWrite(right_enable_pin, HIGH);
-  delay(1000);
-  digitalWrite(right_enable_pin, LOW);
-  delay(1000);
+}
 
+void loop()
+{
+
+  digitalWrite(left_enable_pin, HIGH);
+  delay(1000);
+  digitalWrite(left_enable_pin, LOW);
+  delay(1000);
 }
